bd4a119fc323ae24c25c267b3ea663ac4ef72c3e
Derrick Noutais <derricknoutais@gmail.com>
2019-06-01 20:45:44 +0100

    Fixed Duplicates in api

--- resources/js/components/Contrats/ContratFinal.vue
+++ resources/js/components/Contrats/ContratFinal.vue
@@ -89,7 +89,6 @@
                     <!-- Bouton Envoyer a Cashier -->
                     <button type="button" class="btn btn-primary" @click="envoyerACashier" v-if="contrat_enregistre !== null && contrat.cashier_facture_id === null"> <i v-if="isLoading" class="fas fa-spinner "></i> Envoyer à Cashier</button>
                     <!-- Bouton Voir Dans Cashier -->
-                    
                     <a target="_blank" :href="'https://thecashier.ga/STA/Facture/' + contrat.cashier_facture_id" class="btn btn-primary" v-if="contrat_enregistre !== null && contrat.cashier_facture_id !== null && environment === 'production'" >Voir Facture dans Cashier</a>
                     
                     <a target="_blank" :href="'http://thecashier.test/Lesch%20Group/Facture/' + contrat.cashier_facture_id" class="btn btn-primary" v-if="contrat_enregistre !== null && contrat.cashier_facture_id !== null && environment === 'local'" >Voir Facture dans Cashier</a>
