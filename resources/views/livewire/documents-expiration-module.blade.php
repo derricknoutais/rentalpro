78825accd316b71133d140b20b9ece2510b7340f
Derrick Noutais <derricknoutais@gmail.com>
2022-12-21 12:18:00 +0100

    Documents Exipiration

--- resources/views/livewire/documents-expiration-module.blade.php
+++ resources/views/livewire/documents-expiration-module.blade.php
@@ -2,14 +2,11 @@
 <div class="px-4 sm:px-6 lg:px-8">
     <div class="sm:flex sm:items-center">
         <div class="sm:flex-auto">
-            <h1 class="text-xl font-semibold text-gray-900">Users</h1>
-            <p class="mt-2 text-sm text-gray-700">A list of all the users in your account including their name, title,
-                email and role.</p>
+            <h1 class="text-xl font-semibold text-gray-900">Documents Vehicules</h1>
+            <p class="mt-2 text-sm text-gray-700">Une liste de tous les documents et leur date d'expiration.</p>
         </div>
         <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-            <button type="button"
-                class="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Add
-                user</button>
+
         </div>
     </div>
     <div class="flex flex-col mt-8">
