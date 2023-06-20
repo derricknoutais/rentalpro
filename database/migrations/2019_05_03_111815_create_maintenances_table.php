63508a76e98754fd69936474781d59977b24d0d1
Crishna <mac@MACs-MacBook-Pro-2.local>
2023-06-20 11:33:53 +0100

    Pushing Beta Tests on new Metrics

--- database/migrations/2019_05_03_111815_create_maintenances_table.php
+++ database/migrations/2019_05_03_111815_create_maintenances_table.php
@@ -17,13 +17,13 @@ class CreateMaintenancesTable extends Migration
             $table->unsignedInteger('contractable_id');
             $table->string('contractable_type');
             $table->string('titre')->nullable();
-            $table-> unsignedInteger('technicien_id')->nullable();
+            $table->unsignedInteger('technicien_id')->nullable();
             $table->double('coût')->nullable();
             $table->double('coût_pièces')->nullable();
             $table->unsignedBigInteger('gescash_transaction_id')->nullable();
 
             $table->foreign('voiture_id')->references('id')->on('voitures')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreign('contractable_id')->references('id')->on('contractables')->onDelete('cascade')->onUpdate('cascade');
+            // $table->foreign('contractable_id')->references('id')->on('contractables')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('technicien_id')->references('id')->on('techniciens')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
