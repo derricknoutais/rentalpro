8bab5153d02bac702fb0432e30641893826fa674
derricknoutais <derricknoutais@gmail.com>
2023-07-04 10:33:40 +0100

    upload

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,9 +51,10 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
+
         'public_uploads' => [
             'driver' => 'local',
             'root'   => public_path() . '/uploads',
