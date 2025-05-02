173740f1cfffe6dff95d66d0f7e561e5758a6703
derricknoutais <derricknoutais@gmail.com>
2025-05-02 14:19:36 +0100

    Ajout Module Paiements / Amelioration Process Checkout

--- resources/views/paiements/index.blade.php
+++ resources/views/paiements/index.blade.php
@@ -6,22 +6,16 @@
         <div class="px-4 sm:px-6 lg:px-8">
             <div class="sm:flex sm:items-center">
                 <div class="sm:flex-auto">
-                    <h1 class="text-base font-semibold text-gray-900">Users</h1>
-                    <p class="mt-2 text-sm text-gray-700">A list of all the users in your account including their name,
-                        title, email and role.</p>
-                </div>
-                <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-                    <button type="button"
-                        class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add
-                        user</button>
+                    <h1 class="text-base font-semibold text-gray-900">Paiements</h1>
+                    <p class="mt-2 text-sm text-gray-700">La Liste de tous les Paiements enregistrés</p>
                 </div>
             </div>
             <div class="-mx-4 mt-8 sm:-mx-0">
                 <table class="min-w-full divide-y divide-gray-300">
                     <thead>
                         <tr>
-                            <th scope="col"
-                                class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">Date</th>
+                            <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                                Date</th>
                             <th scope="col"
                                 class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell">
                                 Montant
