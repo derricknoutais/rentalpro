c711208f709005aef66516372f9f1806254c7295
derricknoutais <derricknoutais@gmail.com>
2025-05-03 12:46:15 +0100

    Signature Process First Push

--- database/migrations/2025_05_03_093240_add_settings_columnn_to_users_table.php
+++ database/migrations/2025_05_03_093240_add_settings_columnn_to_users_table.php
@@ -14,14 +14,7 @@ class AddSettingsColumnnToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table
-                ->json('settings')
-                ->nullable()
-                ->default(
-                    json_encode([
-                        'signature' => null,
-                    ]),
-                );
+            $table->json('settings')->nullable();
         });
     }
 
